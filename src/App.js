f90196d90f38dcd40d57185d2fcd295a99085190
John M. Wargo <johnwargo@users.noreply.github.com>
2021-12-03 20:05:29 -0500

    working on project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import buildInfo from './buildInfo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      goodreadsPageCount: 0,
+      actualPagecount: 0,
+      currentPage: 0,
+      adjustedPage: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(`Build Number: ${buildInfo.buildVersion}`);
+    const buildDate = new Date(buildInfo.buildDate);
+    console.log(`Build Date: ${buildDate.toString()}`);
+  }
+
+  handleChange(event) {
+    console.dir(event);
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Build Info</h1>
+        </header>
+        <p>Goodreads is a great application for tracking your reading activity, but its page count for some books is often inaccurate (flat out wrong or including apendices and notes in the page count). In order to get Goodreads to display an acurate completion percentage, you must adjust your current page read to accomodate. This application does it for you.</p>
+        <form>
+          <label>
+            Current Page:
+            <input type="text" id="currentPage" value={this.state.currentPage} onChange={this.handleChange} />
+          </label>
+          <br /><br />
+          <label>
+            Actual Page Count:
+            <input type="text" id="actualPage" value={this.state.actualPagecount} onChange={this.handleChange} />
+          </label>
+          <br /><br />
+          <label>
+            Goodreads Page Count:
+            <input type="text" value={this.state.goodreadsPageCount} onChange={this.handleChange} />
+          </label>
+          <br /><br />
+          <label>
+            Adjusted Page:
+
+          </label>
+          <br /><br />
+        </form>
+        <footer>By John M. Wargo</footer>
+      </div >
+    );
+  }
 }
 
 export default App;
---
+++ src/buildInfo.js
@@ -0,0 +1,4 @@
+module.exports = {
+  buildVersion: "0.0.1",
+  buildDate: 1638577899235,
+}
