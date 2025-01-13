90dfccb2983d56797015703d7c76ec72827b857a
johnwargo <john@johnwargo.com>
2025-01-13 08:15:36 -0500

    converted

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Navbar from 'react-bootstrap/Navbar';
-
 import Swal from 'sweetalert2';
-
+// local modules
 import buildInfo from './buildInfo';
-import './App.css';
 
 // TODO: Validate actual page count is not greater than total page count
+// TODO: convert SWAL2 to Toastify
+
+let dashes = '='.repeat(80);
+let buildDate = new Date(buildInfo.buildDate);
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      goodreadsPageCount: localStorage.getItem('goodreadsPageCount') || 0,
-      actualPageCount: localStorage.getItem('actualPageCount') || 0,
+      pageCountGoodreads: localStorage.getItem('pageCountGoodreads') || 0,
+      pageCountActual: localStorage.getItem('pageCountActual') || 0,
       currentPage: localStorage.getItem('currentPage') || 0,
       adjustedPage: localStorage.getItem('adjustedPage') || 0
     };
@@ -25,14 +24,11 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    let dashes = '='.repeat(80);
-    let buildDate = new Date(buildInfo.buildDate);
-
     console.log(dashes);
-    console.log('Goodreads Page Converter');
+    console.log('Goodreads Page Adjuster');
     console.log('By John M. Wargo (john@johnwargo.com)');
     console.log(`Build: ${buildInfo.buildVersion} - ${buildDate.toString()}`);
-    console.log('(build information generated using my react-build-info package: https://www.npmjs.com/package/react-build-info)');
+    // console.log('(build information generated using my react-build-info package: https://www.npmjs.com/package/react-build-info)');
     console.log(dashes);
 
     this.calculateAdjustedPage();
@@ -42,20 +38,20 @@ class App extends React.Component {
     let adjustedPage;
     if (
       // Do we actually have valid page and pages values?
-      this.state.goodreadsPageCount > 0 &&
-      this.state.actualPageCount > 0 &&
+      this.state.pageCountGoodreads > 0 &&
+      this.state.pageCountActual > 0 &&
       this.state.currentPage > -1) {
       // If the two page counts are the same, ...
-      if (this.state.goodreadsPageCount === this.state.actualPageCount) {
+      if (this.state.pageCountGoodreads === this.state.pageCountActual) {
         // then we have nothing to do, just use the current page as the page number
         adjustedPage = this.state.currentPage;
       } else {
         // otherwise, we need to adjust the current page        
-        let theRatio = 1 / (this.state.actualPageCount / this.state.goodreadsPageCount);
+        let theRatio = 1 / (this.state.pageCountActual / this.state.pageCountGoodreads);
         adjustedPage = Math.floor(this.state.currentPage * theRatio);
       }
       this.setState({ adjustedPage });
-    }   
+    }
   }
 
   handleChange(event) {
@@ -83,66 +79,34 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar bg="light">
-          <Navbar.Brand>
-            <div className="head-text">
-              Goodreads Page Adjuster
-            </div>
-          </Navbar.Brand>
-        </Navbar>
-
-        <div className="page-body">
-          <p>Goodreads is a great application for tracking your reading activity, but its page count for some books is often inaccurate (flat out wrong or including appendices and notes in the page count). In order to get Goodreads to display an accurate completion percentage, you must adjust your current page read to accommodate. This application does it for you.</p>
-
-          <Form>
-            <Form.Group className="mb-3" controlId="currentPage">
-              <Form.Label>Current Page</Form.Label>
-              <Form.Control
-                type="number"
-                min="0"
-                value={this.state.currentPage}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="actualPageCount">
-              <Form.Label>Book (actual) Page Count</Form.Label>
-              <Form.Control
-                type="number"
-                min="0"
-                value={this.state.actualPageCount}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="goodreadsPageCount">
-              <Form.Label>Goodreads Page Count</Form.Label>
-              <Form.Control
-                type="number"
-                min="0"
-                value={this.state.goodreadsPageCount}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="adjustedPage">
-              <Form.Label>Goodreads Page Number (adjusted)</Form.Label>
-              <Form.Control
-                readOnly
-                type="number"
-                value={this.state.adjustedPage}
-              />
-            </Form.Group>
-
-            <Button
-              variant="primary"
-              disabled={!this.state.adjustedPage > 0}
-              onClick={this.handleCopy}>
-              Copy Result
-            </Button>
-          </Form>
-        </div>
-      </div >
+        <main>
+          <section>
+            <h1>Goodreads Page Adjuster</h1>
+            <p>Goodreads is a great application for tracking your reading activity, but its page count for some books is often inaccurate (flat out wrong from including notes, appendices, and other stuff in the page count). In order to get Goodreads to display an accurate completion percentage, you must adjust your current page read to accommodate; this application does it for you.</p>
+            <form>
+              <label for="currentPage">Current Page</label>
+              <input type="number" id="currentPage" name="currentPage" value={this.state.currentPage}
+                onChange={this.handleChange} size="20" />
+              <label for="pageCountActual">Book (actual) Page Count</label>
+              <input type="number" id="pageCountActual" name="pageCountActual" value={this.state.pageCountActual}
+                onChange={this.handleChange} size="20" />
+              <label for="pageCountGoodreads">Goodreads Page Count</label>
+              <input type="number" id="pageCountGoodreads" name="pageCountGoodreads" value={this.state.pageCountGoodreads}
+                onChange={this.handleChange} size="20" />
+              <label for="adjustedPage">Goodreads Adjusted Page Number</label>
+              <input type="number" readonly id="adjustedPage" name="adjustedPage" value={this.state.adjustedPage} size="20" />
+              <br />
+              <button disabled={!this.state.adjustedPage > 0} onClick={this.handleCopy}>Copy Result</button>
+            </form>
+          </section>
+        </main>
+        <footer>
+          <hr />
+          <p>
+            <small><a href="" target="_blank">John M. Wargo</a> | <a href="https://johnwargo.com/posts/2022/accurately-calculating-progress-in-goodreads/" target="_blank">About</a> | <a href="https://github.com/johnwargo/goodreads-page-adjuster" target="_blank">Source Code</a></small>
+          </p>
+        </footer>
+      </div>
     );
   }
 }
--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,4 +1,4 @@
 module.exports = {
   buildVersion: "0.0.6",
-  buildDate: 1736733680644,
+  buildDate: 1736773938030,
 }
